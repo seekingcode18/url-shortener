91d22d2f2cfaa50fbf9882f1bb014e998adfb222
lawcia <lawrencianjume@gmail.com>
2020-02-05 10:30:18 +0000

    error handling

--- project/frontend/views.py
+++ project/frontend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 import requests
+import http
 import json
 
 def index(request):
@@ -8,10 +11,16 @@ def index(request):
 
 def post(request):
     long_url = request.POST.get("long_url", "")
-    response = requests.post('http://localhost:8000/api/urls/', data = {"long_url": str(long_url)})
-    if 'short_url' in response.json():
-        short_url = response.json()['short_url']
-        return render(request, 'index.html', {'short_url': 'http://localhost:8000/{0}'.format(short_url)})
+    try:
+        r = requests.get(long_url)
+        if r.status_code == 200:
+            response = requests.post('http://localhost:8000/api/urls/', data = {"long_url": str(long_url)})
+            if 'short_url' in response.json():
+                short_url = response.json()['short_url']
+                return render(request, 'index.html', {'short_url': 'http://localhost:8000/{0}'.format(short_url)})
+        else:
+            return render(request, 'index.html', {"error": "something went wrong"})
+    except:
+        return render(request,'index.html', {"error": "please enter a valid url"})
 
-    return render(request,'index.html')
-
+    
